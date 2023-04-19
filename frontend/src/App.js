0c773a555c5d7add34b786b1019360cba990779b
Erik Galindo <erikgalindo40@yahoo.com>
2023-04-19 11:31:46 -0700

    update basename prop of Router Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
-    <Router basename='/' >
+    <Router basename='/index.html' >
     <div className="App">
       <Header />
       <Routes>
