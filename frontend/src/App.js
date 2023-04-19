d05c96f5336fe76517efe173fb4615707a24a792
Erik Galindo <erikgalindo40@yahoo.com>
2023-04-19 11:52:33 -0700

    update build script

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
-    <Router basename='/React' >
+    <Router >
     <div className="App">
       <Header />
       <Routes>
