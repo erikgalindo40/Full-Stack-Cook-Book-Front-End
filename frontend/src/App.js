2b4a61f0b2883e688ef73200699cd18587378c2a
Erik Galindo <erikgalindo40@yahoo.com>
2023-04-21 16:02:44 -0700

    change import of BrowserRouter to HashRouter to show endpoints properly on deploy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard';
 import Register from './pages/Register';
 import Login from './pages/Login'
