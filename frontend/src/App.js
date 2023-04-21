324de28b2e8ce2f5132689f0aff541805136274f
Erik Galindo <erikgalindo40@yahoo.com>
2023-04-21 16:08:42 -0700

    add basename to Router Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
-    <Router >
+    <Router basename='/Full-Stack-Cook-Book-Front-End/' >
     <div className="App">
       <Header />
       <Routes>
